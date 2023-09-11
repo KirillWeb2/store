875c05e56593ef541d3dd3a0526f0af70ab731ce
Kirill <kirill2004pro1000@mail.ru>
2023-09-11 22:19:57 +1000

    bug fix, for deployment to vercel

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,12 @@ export default authMiddleware({
     publicRoutes: [
         '/api/(.*)',
     ],
-    debug: true,
-    jwksCacheTtlInMs: 60 * 60 * 1000,
-    clockSkewInMs: 5 * 60 * 1000,
 });
 
 export const config = {
     matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+// debug: true,
+// jwksCacheTtlInMs: 60 * 60 * 1000,
+// clockSkewInMs: 5 * 60 * 1000,
