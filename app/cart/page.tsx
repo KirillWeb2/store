855da56871a8181555b1fbf7799afe89b86f59c1
Kirill <kirill2004pro1000@mail.ru>
2023-09-11 21:56:05 +1000

    fix

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,12 +1,9 @@
 import { CartList } from '@/components/cart';
 import { CartLayout, OrderLayout } from '@/components/layout';
-import { cartService } from '@/service/cart-service';
 
 export default async function CartPage() {
-    const { cart } = await cartService.getCart();
-    
     return (
-        <CartLayout cart={cart}>
+        <CartLayout>
             <OrderLayout isAdmin={false}>
                 <CartList />
             </OrderLayout>
