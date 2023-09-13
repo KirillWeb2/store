1df619d6710a5aa5b9e0df667cc639755f8723ef
Kirill <kirill2004pro1000@mail.ru>
2023-09-13 22:01:35 +1000

    update server url

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        // SERVER_URL: 'https://store-pearl-phi.vercel.app/api',
-        SERVER_URL: 'http://localhost:3000/api',
+        SERVER_URL: 'https://store-pearl-phi.vercel.app/api',
+        // SERVER_URL: 'http://localhost:3000/api',
     },
 };
 
